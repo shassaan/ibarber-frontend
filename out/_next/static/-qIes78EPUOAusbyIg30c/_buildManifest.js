1e791e6fd0fe2ba8d52020cd21c25d756eb0c9cb
Syed Hassaan Ahmed <syedhassaan.103@gmail.com>
2021-01-30 00:16:50 +0500

    created build

---
+++ out/_next/static/-qIes78EPUOAusbyIg30c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6a35b8a9bdb936f28109.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-276bd7231f08cd5de291.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
